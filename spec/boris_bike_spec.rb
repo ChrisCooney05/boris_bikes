26158ccb3ba6749f9435671aaef8a17a7c72b2ea
Tristan Langford <tristanlangford@hotmail.com>
2020-05-12 14:54:15 +0100

    step 8 complete

--- spec/boris_bike_spec.rb
+++ spec/boris_bike_spec.rb
@@ -1,4 +1,9 @@
+require 'DockingStation'
 
-describe "DockingStation" do
+describe DockingStation do
+
+  it 'respond to release bike' do
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
 
 end
