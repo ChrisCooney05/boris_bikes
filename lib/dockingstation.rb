77198bc691a681703350c0784b63a3502ba441d8
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-12 15:13:02 +0100

    Step 9 complete

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -3,4 +3,11 @@ class DockingStation
   def release_bike
 
   end
+
+end
+
+class Bike
+  def working?
+  end 
+
 end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,8 @@
+require 'dockingstation'
+
+describe Bike do
+  it 'responds to working?' do
+    expect(Bike.new).to respond_to(:working?)
+  end
+
+end
